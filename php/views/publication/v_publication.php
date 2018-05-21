ce0f13c3667e813e3a7d1c35ae8f641307a9dbea
Tom <thomas.scutt@gmail.com>
2018-05-21 14:05:05 +0100

    Update v_publication.php

--- php/views/publication/v_publication.php
+++ php/views/publication/v_publication.php
@@ -111,9 +111,7 @@
 							</name>
 							<aat_id>
 								<?if(!empty($tag->aat_search)):?>
-								<aat_id>
 									<?=htmlspecialchars($tag->aat_search)?>
-									</aat_id>
 								<?endif?>
 							</aat_id>
 							<ulan_id>
